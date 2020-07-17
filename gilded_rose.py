8e1eb8b04f1c3afe22cfb00cd14e7ebd53820187
Tomas Račys <tomas@versada.eu>
2020-07-17 15:13:12 +0300

    Refactor GildedRose implementation, Add new classes of items, Fix tests

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,39 +1,15 @@
 # -*- coding: utf-8 -*-
 
 class GildedRose(object):
-
     def __init__(self, items):
         self.items = items
 
-    def update_quality(self):
-        for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+    def update(self, days=1):
+        while days > 0:
+            days -= 1
+
+            for item in self.items:
+                item.update()
 
 
 class Item:
@@ -44,3 +20,73 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+
+class ItemWrapped(Item):
+    def __init__(self, name, sell_in, quality):
+        if sell_in < 0:
+            raise Exception(
+                'Cannot add expired items to inventory'
+            )
+
+        # Using old-style super, because task description
+        # mentions that Item cannot be touched
+        Item.__init__(self, name, sell_in, quality)
+        self.quality = min(self.quality, 50)
+
+    def update(self):
+        self._update_sell_in()
+        self._update_quality()
+
+        if self.quality < 0:
+            self.quality = 0
+        if self.quality > 50:
+            self.quality = 50
+
+    def _update_sell_in(self):
+        self.sell_in -= 1
+
+    def _update_quality(self):
+        raise NotImplementedError
+
+
+class ItemStatic(ItemWrapped):
+    def _update_sell_in(self):
+        pass
+
+    def _update_quality(self):
+        pass
+
+
+class ItemRegular(ItemWrapped):
+    def _update_quality(self):
+        self.quality -= 1
+        if self.sell_in < 0:
+            self.quality -= 1
+
+
+class ItemConjured(ItemWrapped):
+    def _update_quality(self):
+        self.quality -= 2
+        if self.sell_in < 0:
+            self.quality -= 2
+
+
+class ItemAgedBrie(ItemWrapped):
+    def _update_quality(self):
+        self.quality += 1
+        if self.sell_in < 0:
+            self.quality += 1
+
+
+class ItemBackstagePass(ItemWrapped):
+    def _update_quality(self):
+        if self.sell_in < 0:
+            self.quality = 0
+            return
+
+        self.quality += 1
+        if self.sell_in <= 10:
+            self.quality += 1
+        if self.sell_in <= 5:
+            self.quality += 1
--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -1,14 +1,113 @@
 # -*- coding: utf-8 -*-
+
 import unittest
 
-from gilded_rose import Item, GildedRose
+from gilded_rose import (
+    GildedRose,
+    ItemRegular,
+    ItemStatic,
+    ItemConjured,
+    ItemAgedBrie,
+    ItemBackstagePass,
+)
+
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+    def test_regular_item(self):
+        item = ItemRegular(
+            "Corn", sell_in=10, quality=10,
+        )
+
+        rose = GildedRose([item])
+        rose.update()
+        self.assertEqual(
+            item.sell_in, item.quality, 9,
+        )
+
+        rose.update(days=9)
+        self.assertEqual(
+            item.sell_in, item.quality, 0,
+        )
+
+        rose.update(days=100)
+        self.assertEqual(item.sell_in, -100)
+        self.assertEqual(item.quality, 0)
+
+    def test_static_item(self):
+        item = ItemStatic(
+            "Sulfuras", sell_in=10, quality=9000,
+        )
+
+        GildedRose([item]).update(days=100)
+        self.assertEqual(item.sell_in, 10)
+        self.assertEqual(item.quality, 50)
+
+    def test_conjured_item(self):
+        item = ItemConjured(
+            "Sword", sell_in=20, quality=100,
+        )
+
+        rose = GildedRose([item])
+        rose.update()
+        self.assertEqual(item.sell_in, 19)
+        self.assertEqual(item.quality, 48)
+
+        rose.update(days=19)
+        self.assertEqual(item.sell_in, 0)
+        self.assertEqual(item.quality, 10)
+
+        rose.update()
+        self.assertEqual(item.sell_in, -1)
+        self.assertEqual(item.quality, 6)
+
+    def test_brie_item(self):
+        item = ItemAgedBrie(
+            "Cheese", sell_in=10, quality=5,
+        )
+
+        rose = GildedRose([item])
+        rose.update()
+        self.assertEqual(item.sell_in, 9)
+        self.assertEqual(item.quality, 6)
+
+        rose.update(days=9)
+        self.assertEqual(item.sell_in, 0)
+        self.assertEqual(item.quality, 15)
+
+        rose.update()
+        self.assertEqual(item.sell_in, -1)
+        self.assertEqual(item.quality, 17)
+
+    def test_backstage_pass_item(self):
+        item = ItemBackstagePass(
+            "Backstage Pass", sell_in=20, quality=10,
+        )
+
+        rose = GildedRose([item])
+        rose.update()
+        self.assertEqual(item.sell_in, 19)
+        self.assertEqual(item.quality, 11)
+
+        rose.update(days=8)
+        self.assertEqual(item.sell_in, 11)
+        self.assertEqual(item.quality, 19)
+
+        rose.update()
+        self.assertEqual(item.sell_in, 10)
+        self.assertEqual(item.quality, 21)
+
+        rose.update(days=4)
+        self.assertEqual(item.sell_in, 6)
+        self.assertEqual(item.quality, 29)
+
+        rose.update()
+        self.assertEqual(item.sell_in, 5)
+        self.assertEqual(item.quality, 32)
+
+        rose.update(days=100000)
+        self.assertEqual(item.sell_in, -99995)
+        self.assertEqual(item.quality, 0)
+
 
 if __name__ == '__main__':
     unittest.main()
